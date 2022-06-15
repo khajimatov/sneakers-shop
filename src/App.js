cb4394f76e1a4ee2461f378491f322d6db8544b9
Bakha <for.official.bkh@gmail.com>
2022-06-15 16:09:18 +0600

    Added Sneaker cards and search field

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <ul>
           <li>
             <img width={20} height={20} src="/img/group.svg" alt="Group"></img>
-            <span>1205 USD.</span>
+            <span>$1205 USD.</span>
           </li>
           <li>
             <img
@@ -35,8 +35,186 @@ function App() {
         </ul>
       </header>
       <div className="content">
-        <h1>All sneakers</h1>
-        <div className="sneakers"></div>
+        <div className="d-flex justify-between align-center">
+          <h1>All sneakers</h1>
+          <div className="search-block align-center d-flex cu-p" for="search">
+            <label for="search">
+              <img
+                className="cu-p"
+                width={16}
+                height={16}
+                src="/img/search.svg"
+                alt="Search Icon"
+              />
+            </label>
+            <input
+              autocomplete="off"
+              type="text"
+              placeholder="Search..."
+              name="search"
+              id="search"
+            />
+          </div>
+        </div>
+
+        <div className="sneakers">
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/11.jpg"
+              alt="Nike Blazer Mid Suede for Men"
+            />
+            <h4>Nike Blazer Mid Suede for Men</h4>
+            <div className="cardinfo">
+              <div>
+                <h6>PRICE:</h6>
+                <b>$120 USD</b>
+              </div>
+              <button>
+                <svg
+                  alt="Plus Sign"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="0.5"
+                    y="0.5"
+                    width="31"
+                    height="31"
+                    rx="7.5"
+                    fill="white"
+                    stroke="#F2F2F2"
+                  />
+                  <path
+                    d="M20.6653 15.1312H17.2021V11.6682C17.2021 10.3328 15.1311 10.3328 15.1311 11.6682V15.1312H11.668C10.3329 15.1312 10.3329 17.2022 11.668 17.2022H15.1311V20.6652C15.1311 22.0005 17.2021 22.0005 17.2021 20.6652V17.2022H20.6653C22.0005 17.2022 22.0005 15.1312 20.6653 15.1312Z"
+                    fill="#D3D3D3"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/1.jpg"
+              alt="Puma X Aka Boku Future Rider"
+            />
+            <h4>Puma X Aka Boku Future Rider</h4>
+            <div className="cardinfo">
+              <div>
+                <h6>PRICE:</h6>
+                <b>$188 USD</b>
+              </div>
+              <button>
+                <svg
+                  alt="Plus Sign"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="0.5"
+                    y="0.5"
+                    width="31"
+                    height="31"
+                    rx="7.5"
+                    fill="white"
+                    stroke="#F2F2F2"
+                  />
+                  <path
+                    d="M20.6653 15.1312H17.2021V11.6682C17.2021 10.3328 15.1311 10.3328 15.1311 11.6682V15.1312H11.668C10.3329 15.1312 10.3329 17.2022 11.668 17.2022H15.1311V20.6652C15.1311 22.0005 17.2021 22.0005 17.2021 20.6652V17.2022H20.6653C22.0005 17.2022 22.0005 15.1312 20.6653 15.1312Z"
+                    fill="#D3D3D3"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/12.jpg"
+              alt="Nike Air Max 270"
+            />
+            <h4>Nike Air Max 270</h4>
+            <div className="cardinfo">
+              <div>
+                <h6>PRICE:</h6>
+                <b>$95 USD</b>
+              </div>
+              <button>
+                <svg
+                  alt="Plus Sign"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="0.5"
+                    y="0.5"
+                    width="31"
+                    height="31"
+                    rx="7.5"
+                    fill="white"
+                    stroke="#F2F2F2"
+                  />
+                  <path
+                    d="M20.6653 15.1312H17.2021V11.6682C17.2021 10.3328 15.1311 10.3328 15.1311 11.6682V15.1312H11.668C10.3329 15.1312 10.3329 17.2022 11.668 17.2022H15.1311V20.6652C15.1311 22.0005 17.2021 22.0005 17.2021 20.6652V17.2022H20.6653C22.0005 17.2022 22.0005 15.1312 20.6653 15.1312Z"
+                    fill="#D3D3D3"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/9.jpg"
+              alt="Nike Blazer Mid Suede"
+            />
+            <h4>Nike Blazer Mid Suede</h4>
+            <div className="cardinfo">
+              <div>
+                <h6>PRICE:</h6>
+                <b>$215 USD</b>
+              </div>
+              <button>
+                <svg
+                  alt="Plus Sign"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="0.5"
+                    y="0.5"
+                    width="31"
+                    height="31"
+                    rx="7.5"
+                    fill="white"
+                    stroke="#F2F2F2"
+                  />
+                  <path
+                    d="M20.6653 15.1312H17.2021V11.6682C17.2021 10.3328 15.1311 10.3328 15.1311 11.6682V15.1312H11.668C10.3329 15.1312 10.3329 17.2022 11.668 17.2022H15.1311V20.6652C15.1311 22.0005 17.2021 22.0005 17.2021 20.6652V17.2022H20.6653C22.0005 17.2022 22.0005 15.1312 20.6653 15.1312Z"
+                    fill="#D3D3D3"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
